74ca5c245647b3ada667ab0937f23c7590fefe34
Arlème <60097202+rootkit7628@users.noreply.github.com>
2022-12-29 19:31:35 +0000

    Should reinit  the var in loop

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,25 @@
-use std::io;
 use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Atsika tsika hihisa !");
 
     let secret_number = rand::thread_rng().gen_range(1..=10);
-    println!("Firy me gne sifranao.");
-
-    let mut guess = String::new();
 
     loop {
+        println!("Firy me gne sifranao.");
+
+        let mut guess = String::new();
+
         io::stdin()
             .read_line(&mut guess)
             .expect("Eee! nagnadary koa i endrinira tiky");
 
-        let guess: u32 = guess.trim().parse().expect("Tsy henonao me nge raha zakaiko ake, ino hoa gne sifranao !");
+        let guess: u32 = guess
+            .trim()
+            .parse()
+            .expect("Tsy henonao me nge raha zakaiko ake, ino hoa gne sifranao !");
 
         println!("nge nomenao {guess} anah ake.");
 
